b7b8fd39d041ab845c082db709e9c57a6e3271c5
Karłowski Bartosz <bartosz.karlowski@audiotekagroup.com>
2024-07-15 16:37:45 +0200

    first handler

--- api/raptor/user.go
+++ api/raptor/user.go
@@ -1,1 +1,29 @@
 package raptor
+
+import (
+	"github.com/barkar96/raptor/lib/api"
+	"github.com/barkar96/raptor/lib/logging"
+	"github.com/gofiber/fiber/v2"
+)
+
+var _ api.API = (*UserAPI)(nil)
+
+type UserAPI struct{}
+
+func NewUserAPI() UserAPI {
+	return UserAPI{}
+}
+
+func (u UserAPI) Register(app *fiber.App) error {
+	apiV1 := app.Group("/api/v1")
+
+	apiV1.Post("/user", u.HandleCreateUser)
+
+	return nil
+}
+
+func (u UserAPI) HandleCreateUser(c *fiber.Ctx) error {
+	ctx := c.Context()
+	logging.Info(ctx, "hello world")
+	return nil
+}
--- cmd/raptor/main.go
+++ cmd/raptor/main.go
@@ -1,1 +1,56 @@
-package raptor
+package main
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/barkar96/raptor/cmd"
+	"github.com/barkar96/raptor/cmd/worker/config"
+	"github.com/barkar96/raptor/lib/actor"
+	"github.com/barkar96/raptor/lib/api"
+	"github.com/barkar96/raptor/lib/logging"
+	"github.com/barkar96/raptor/lib/redis"
+
+	"github.com/barkar96/raptor/api/raptor"
+)
+
+func main() {
+	cmd.SetGoMaxProcs()
+	ctx := context.Background()
+	cfg := config.GetInstance()
+	logging.Init(slog.LevelInfo, cfg.EnvironmentName, cfg.ApplicationName, false)
+
+	if err := run(ctx, cfg); err != nil {
+		logging.WithFatalError(ctx, err, "application stopped on error")
+	}
+}
+
+func run(ctx context.Context, cfg *config.Config) error {
+	g := actor.New()
+
+	// === Redis ===
+	rdb, err := redis.New(cfg.RedisAddr, cfg.RedisPassword, cfg.RedisTimeout)
+	if err != nil {
+		logging.WithFatalError(ctx, err, "Redis initilization failed")
+	}
+	logging.Info(ctx, "Redis initialized")
+	g.Add(rdb)
+
+	// === PostgreSQL ===
+
+	// === Fiber ===
+	userAPI := raptor.NewUserAPI()
+	app, err := api.NewAPI("0.0.0.0:8080", "dev", &userAPI)
+	if err != nil {
+		logging.WithFatalError(ctx, err, "API initilization failed")
+	}
+	g.Add(app)
+
+	err = g.Run(ctx, 5*time.Second)
+	if err != nil {
+		logging.WithError(ctx, err, "application stopped on error")
+	}
+
+	return nil
+}
